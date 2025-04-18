3d1f1d128d3f79121383a61ce943cb97c366e057
ochiecommerce <ochieaustine98@gmail.com>
2025-04-18 22:30:00 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,4 +3,4 @@ from Cython.Build import cythonize
 from glob import glob 
 files = glob("*.py")
 targetf = [f for f in files if f not in ["setup.py","yt-worker.py","ke-server.py"]]
-setup(ext_modules=cythonize(targetf))
+setup(ext_modules=cythonize(targetf,compiler_directives={'language_level': '3'}))
